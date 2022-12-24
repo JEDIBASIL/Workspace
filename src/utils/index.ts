2e7d512a64305c8df73d6aaa72d2ec52430b7007
JEDIBASIL <jedidiahbasil@gmail.com>
2022-12-24 19:35:33 +0100

    created function to be able to save notes to localstorage, get notes  and check if note exits

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,25 +1,19 @@
-type Note = {
-    id: string;
-    title: string;
-    text: string;
-    favorite: boolean;
-    date_created: Date;
-} | null;
+import { Note } from "../types";
 
 const saveNote = (note: Note[]): boolean => {
     localStorage.setItem("notes", JSON.stringify(note))
     return true;
 }
 
-const isNote = ():boolean =>{
+const isNote = (): boolean => {
     const isSet = localStorage.getItem("notes")
-    return isSet?true :false;
+    return isSet ? true : false;
 }
 
-const getNote = ():Note[] | any =>{
+const getNote = (): Note[] | any => {
     const isSet = localStorage.getItem("notes")
-    if(isSet)
-    return JSON.parse(isSet);
+    if (isSet)
+        return JSON.parse(isSet);
 }
 
-export { saveNote }
+export { saveNote, isNote, getNote }
