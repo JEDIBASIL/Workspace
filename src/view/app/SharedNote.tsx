be43f3640522b1688b578cf9a29a7a11a42996ef
JEDIBASIL <jedidiahbasil@gmail.com>
2022-12-24 19:37:05 +0100

    created a page for shared notes

---
+++ src/view/app/SharedNote.tsx
@@ -0,0 +1,35 @@
+import { Tabs } from "@mantine/core";
+
+const SharedNote: React.FC = () => {
+  return (
+    <>
+      <div className="notes_container">
+        <h2>Shared notes</h2>
+        <Tabs variant="outline" defaultValue="your_note">
+          <Tabs.List>
+            <Tabs.Tab value="your_note">
+              Your notes
+            </Tabs.Tab>
+            <Tabs.Tab value="others_note">
+              Others notes
+            </Tabs.Tab>
+          </Tabs.List>
+
+          <Tabs.Panel value="gallery" pt="xs">
+            Gallery tab content
+          </Tabs.Panel>
+
+          <Tabs.Panel value="messages" pt="xs">
+            Messages tab content
+          </Tabs.Panel>
+
+          <Tabs.Panel value="settings" pt="xs">
+            Settings tab content
+          </Tabs.Panel>
+        </Tabs>
+      </div>
+    </>
+  );
+};
+
+export default SharedNote;
