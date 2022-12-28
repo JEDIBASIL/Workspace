f682f595c78444e2a728daf0e37df4a109163a0f
JEDIBASIL <jedidiahbasil@gmail.com>
2022-12-28 01:13:26 +0100

    modified test for EventList component

--- src/components/layouts/EventList.tsx
+++ src/components/layouts/EventList.tsx
@@ -1,5 +1,5 @@
 import { Menu } from "@mantine/core";
-import { TfiHeart, TfiMore, TfiTrash } from "react-icons/tfi";
+import { TfiCalendar, TfiClose } from "react-icons/tfi";
 
 interface EventListProps {
     time: string;
@@ -11,25 +11,13 @@ const EventList: React.FC<EventListProps> = ({ time, event }) => {
         <>
             <li>
                 <div className="event_details">
-                    <h5>{time}</h5>
-                    <p>{event}</p>
+                    <h5>{event}</h5>
+                    <p role={"para"}>{time}</p>
                 </div>
-                <Menu withArrow position="bottom-end" shadow="md" width={250}>
-                    <Menu.Target>
-                        <button className="more">
-                            <TfiMore />
-                        </button>
-                    </Menu.Target>
-
-                    <Menu.Dropdown>
-                        <Menu.Item icon={<TfiTrash />}>
-                            <p style={{ fontWeight: "500" }}>Delete </p>
-                        </Menu.Item>
-                        <Menu.Item icon={<TfiHeart />}>
-                            <p style={{ fontWeight: "500" }}></p>
-                        </Menu.Item>
-                    </Menu.Dropdown>
-                </Menu>
+                <div className="action_container">
+                    <button className="more"><TfiCalendar /></button>
+                    <button className="more"><TfiClose /></button>
+                </div>
             </li></>
     );
 }
